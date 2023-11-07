2886e0df6d80532d0b3c6a2d3a9d8acaf0744c67
filecc <fil.gia@live.it>
2023-11-07 10:55:32 +0100

    added nodemon dependencies and gitignore

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8080
 const host = process.env.HOST || 'localhost'
 
 const server = http.createServer((req, res) => {
-    res.end('<h1>Hello World</h1>')
+    res.end('<h1>Hello World!</h1>')
 }).listen(port, host, () => {
     console.log('Server started on: ' + 'http://'+ host +':'+ port);
 });
