d4641cf7dc89adfa7a3efa91bf6bf057f92dc42c
filecc <fil.gia@live.it>
2023-11-07 11:43:33 +0100

    added env

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const http = require('http')
 const port = process.env.PORT || 8080
 const host = process.env.HOST || 'localhost'
 
+
 const server = http.createServer((req, res) => {
+   
     res.end('<h1>Hello World!</h1>')
 }).listen(port, host, () => {
     console.log('Server started on: ' + 'http://'+ host +':'+ port);
