3858f77c96347aec1976358711ab984b8bd8f686
filecc <fil.gia@live.it>
2023-11-07 10:28:01 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const port = process.env.PORT || 8080
+const host = process.env.HOST || 'localhost'
+
+const server = http.createServer((req, res) => {
+    res.end('<h1>Hello World</h1>')
+}).listen(port, host, () => {
+    console.log('Server started on: ' + 'http://'+ host +':'+ port);
+});
